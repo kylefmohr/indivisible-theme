cdd7a8512801eef4522d78540eeecdcbfbaa36d0
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2026-04-08 22:23:18 -0500

    idk

--- extra/custom-head.blade.php
+++ extra/custom-head.blade.php
@@ -16,4 +16,31 @@ Down below, you can find a few examples using this function:
 --}}
 
 <link rel="stylesheet" href="{{themeAsset('your.css')}}">
-<style>body{background-image: url('{{themeAsset('background.png')}}'); background-size: cover; background-position: center; background-attachment: fixed;}</style>
+<style>
+body{background-image: url('{{themeAsset('background.png')}}'); 
+background-size: cover; 
+background-position: center; 
+background-attachment: fixed;}
+
+
+
+
+
+html {
+    font-size: 100%; }
+      body {
+        font-size: 18px;
+        line-height: 24px;
+        font-weight: 400;
+        color: #F1F1F1  !important;
+        font-family: "sottext", sans-serif;
+        text-transform: uppercase;
+        background:
+        linear-gradient(
+          rgba(0, 0, 0, 0.7), 
+          rgba(0, 0, 0, 0.7)
+        ),
+		url({{themeAsset('background.png')}});
+        background-size: cover;
+        color: #222; }
+</style>
